51e1da63b57f60dc8d7a1b7a03a66a78de202463
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2023-01-18 12:47:58 +0400

    init base project

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by IKAKOOO on 18.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
