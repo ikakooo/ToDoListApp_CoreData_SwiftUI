89294a34d1a8ddc126d22ce1da43f365efcecdb2
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2023-01-20 11:02:37 +0400

    code clean up and managing

---
+++ ToDoList/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  ToDoList
+//
+//  Created by IKAKOOO on 20.01.23.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
